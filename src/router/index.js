347e4d32882fb67d2bdec538e0235a381cdb1111
bartbdx <pl_dtm@hotmail.com>
2018-02-09 11:17:35 +0100

    change helloword.vue

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Ticket from '@/components/Ticket'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Ticketing',
+      component: Ticket
     }
   ]
 })
