edfe1db5d0729f308afb195153f52314f9f8b60b
bartbdx <pl_dtm@hotmail.com>
2018-02-09 15:44:30 +0100

    affichage table de base

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,15 +1,78 @@
 <template>
-  <div class="hello">
+  <div>
     <h2>{{msg}}</h2>
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <td id="example-1" v-for="column in columns">
+              {{ column.field }}
+          </td>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="row in rows">
+          <td id="example-2" v-for="col in row">
+            {{ col}}
+          </td>
+        </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
+
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  demandeur: 'Ticketing',
+
   data () {
     return {
-      msg: 'Ticketing App'
+      msg: 'Ticketing App',
+      columns: [
+        {
+          label: 'Ticket',
+          field: 'ticket',
+          filterable: true,
+          type: 'number'
+        },
+        {
+          label: 'Date',
+          field: 'date',
+          type: 'date',
+          inputFormat: 'DDMMYYYY',
+          filterable: true
+        },
+        {
+          label: 'Demandeur',
+          field: 'demandeur',
+          filterable: true
+        },
+        {
+          label: 'Objet',
+          field: 'objet',
+          filterable: true
+        },
+        {
+          label: 'Detail',
+          field: 'detail',
+          filterable: true
+        }
+      ],
+      rows: [
+        {ticket: 1, date: '24-11-2017', demandeur: 'Tom', objet: 'objet', detail: 'detail operation'},
+        {ticket: 2, date: '24-11-2017', demandeur: 'Jean', objet: 'objet', detail: 'detail operation'},
+        {ticket: 3, date: '24-11-2017', demandeur: 'Marie', objet: 'objet', detail: 'detail operation'},
+        {ticket: 4, date: '24-11-2017', demandeur: 'Chris', objet: 'objet', detail: 'detail operation'},
+        {ticket: 5, date: '24-11-2017', demandeur: 'Jean', objet: 'objet', detail: 'detail operation'},
+        {ticket: 6, date: '24-11-2017', demandeur: 'Marie', objet: 'objet', detail: 'detail operation'},
+        {ticket: 7, date: '24-11-2017', demandeur: 'Tom', objet: 'objet', detail: 'detail operation'},
+        {ticket: 8, date: '24-11-2017', demandeur: 'Chris', objet: 'objet', detail: 'detail operation'},
+        {ticket: 9, date: '24-11-2017', demandeur: 'Chris', objet: 'objet', detail: 'detail operation'},
+        {ticket: 10, date: '24-11-2017', demandeur: 'Tom', objet: 'objet', detail: 'detail operation'}
+      ]
+
     }
   }
 }
@@ -17,18 +80,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
