24d79a74de8f362b8f18c87b3f1556a344023202
bartbdx <pl_dtm@hotmail.com>
2018-02-09 11:25:32 +0100

    naming

--- src/components/Ticket.vue
+++ src/components/Ticket.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="hello">
     <h2>{{msg}}</h2>
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'Ticket',
   data () {
     return {
       msg: 'Ticketing App'
